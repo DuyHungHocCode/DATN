5b7f1642b6f59747429510a29870b5d862c27c55
DuyHungHocCode <duyhungpham0209@gmail.com>
2025-06-13 15:54:10 +0700

    structural change

--- service/be/citizen_service_bca/app/schemas/reference.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List, Dict, Any, Optional
-
-class ReferenceDataResponse(BaseModel):
-    data: Dict[str, List[Dict[str, Any]]]
-    total_tables: int
-    
-    class Config:
-        orm_mode = True
